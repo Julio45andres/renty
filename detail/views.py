0845ac4708d719c9927e13cb55c5f275b427762d
Julio45andres <julian.munozm@udea.edu.co>
2018-11-09 15:17:01 -0500

    REST API

--- detail/views.py
+++ detail/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.views import APIView
+from rest_framework import status, generics
+from rest_framework.response import Response
+from .models import Rental, Car
+from .serializers import RentalSerializer, CarSerializer
 
-# Create your views here.
+
+class RentalView(generics.ListAPIView):
+    serializer_class = RentalSerializer
+
+    def get_queryset(self):
+        queryset = Rental.objects.all()
+        return queryset
+
+    def post(self, request):
+        serializer = RentalSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CarView(generics.ListAPIView):
+    serializer_class = CarSerializer
+
+    def get_queryset(self):
+        queryset = Car.objects.all()
+        return queryset
+
+    def post(self, request):
+        serializer = CarSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- renty/settings.py
+++ renty/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps renty
+    'detail.apps.DetailConfig',
+    # Framework REST django
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
