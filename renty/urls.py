30194e7ecc0a3263d0161abe59a53c339e9a4eca
johnathan.monsalve <johnathan.monsalve@udea.edu.co>
2018-11-17 10:01:44 -0500

    change get cars/id

--- renty/urls.py
+++ renty/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     # path('', views.CarView.as_view()),
     path('admin/', admin.site.urls),
     url(r'^rentals/', views.RentalView.as_view()),
-    url(r'^cars/', views.CarView.as_view()),
+    url(r'^cars/$', views.CarViewId.as_view()),
     url(r'^cars/(?P<carid>[-\w]+)/$', views.CarViewId.as_view()),
     url(r'^cars/search$', views.CarSearchView.as_view())
 ]
