72759dba4d065d9451dc9d53a9e71fb86a433404
Julio45andres <julian.munozm@udea.edu.co>
2018-12-10 23:35:41 -0500

    Pictures added to test

--- detail/tests.py
+++ detail/tests.py
@@ -19,11 +19,15 @@ class BookingTest(TestCase):
     """ Test module for booking model """
 
     def setUp(self):
+		pictures = [
+            "https://c1.staticflickr.com/9/8728/16671205057_889930d3c0_b.jpg",
+            "http://www.mansory.com/files/styles/mansory_range_overview_main/public/media/cars/bugatti_veyron_16.4/linea_vivere/MANSORY_vivere_ext_06.jpg?itok=oDKbqzwI"
+        ]
         rental = CarRental.objects.create(_id=1, name="f")
         car1 = Car.objects.create(brand="ff", thumbnail="ff", price=3, category="SUV",
-                                  model="2019", pickup="Aeropuerto", rental=rental, plate=23, rating=3, capacity=4, transmission="Mecanica", doors=5, color="Azul", kms=1)
+                                  model="2019", pickup="Aeropuerto", rental=rental, plate=23, rating=3, capacity=4, transmission="Mecanica", doors=5, color="Azul", kms=1, pictures=pictures)
         car2 = Car.objects.create(brand="ff", thumbnail="ff", price=3, category="SUV",
-                                  model="2019", pickup="Aeropuerto", rental=rental, plate=11, rating=3, capacity=4, transmission="Mecanica", doors=5, color="Azul", kms=1)
+                                  model="2019", pickup="Aeropuerto", rental=rental, plate=11, rating=3, capacity=4, transmission="Mecanica", doors=5, color="Azul", kms=1, pictures=pictures)
         CarRent.objects.create(car=car1, token="eREw", uidUser=45, bookingDate=_parse_date("2018-11-23"), pickup="Aeropuerto",
                                pickupDate=_parse_date("2018-11-25"), deliverPlace="Parque del poblado", deliverDate=_parse_date("2018-11-27"), rental=rental)
         CarRent.objects.create(
